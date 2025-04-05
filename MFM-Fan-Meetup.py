fb108c76fdaa43bf07fd865ed2080ce7b29f4dfa
LilianReanda <60326719+LilianReanda@users.noreply.github.com>
2025-04-05 12:57:59 -0400

    Delete MFM-Fan-Meetup.py

--- MFM-Fan-Meetup.py
+++
@@ -1,5 +0,0 @@
-import requests
-url = "https://app.getriver.io/beta/events/mfm-fan-meetup-nyc-8i6"
-headers = {"User-Agent": "Mozilla/5.0"}
-response = requests.get(url, headers=headers)
-print(response.text)  # Check if the event details appear
