008113f78a3e7c401fb5ca6f474067bf117f539a
LilianReanda <lilianreanda@gmail.com>
2025-04-06 18:37:11 -0400

    Merge remote-tracking branch 'origin/main'

--- luma.py
+++ luma.py
@@ -1,21 +1,42 @@
 import requests
+import json
+
 
 def deal_with_data():
-	for entry in json["entries"]:
-		print(entry["event"]["name"])
+    for entry in res_json["entries"]:
+        event_list = {}
+        event_list["name"] = entry["event"]["name"]
+        event_list["start_time"] = entry["event"]["start_at"]
+        event_list["timezone"] = entry["event"]["timezone"]
+        try:
+            event_list["address"] = entry["event"]["geo_address_info"]["full_address"]
+        except:
+            event_list["address"] = None
+
+        event_list["guest_count"] = entry["guest_count"]
+
+        # event_list["host"] = entry["hosts"]
+        event_list["hosts"] = []
+        for host in entry["hosts"]:
+            event_list["hosts"].append(host["name"])
+
+        all_events_list.append(event_list)
+
 
 # Main URL
 # url = "https://lu.ma/nyc"
 
+all_events_list = []
+
 # Paginated URL
 url = "https://api.lu.ma/discover/get-paginated-events"
 params = {
-			"discover_place_api_id": "discplace-Izx1rQVSh8njYpP",
-			"pagination_cursor": ""
-		}
+    "discover_place_api_id": "discplace-Izx1rQVSh8njYpP",
+    "pagination_cursor": ""
+}
 
 r = requests.request("GET", url, params=params)
-json = r.json()
+res_json = r.json()
 
 # for entry in json["entries"]:
 # 	print(entry["event"]["name"])
@@ -23,12 +44,18 @@ deal_with_data()
 
 while True:
 
-	if json["has_more"]:
-		params = {
-			"discover_place_api_id": "discplace-Izx1rQVSh8njYpP",
-			"pagination_cursor": json["next_cursor"]}
-		r = requests.request("GET", url, params=params)
-		json = r.json()
-		deal_with_data()
-	else:
-		break
+    if res_json["has_more"]:
+        params = {
+            "discover_place_api_id": "discplace-Izx1rQVSh8njYpP",
+            "pagination_cursor": res_json["next_cursor"]}
+        r = requests.request("GET", url, params=params)
+        res_json = r.json()
+        deal_with_data()
+    else:
+        break
+
+# final_json = json.dumps(all_events_list)
+# print(final_json)
+
+with open('luma-data.json', 'w', encoding='utf-8') as f:
+    json.dump(all_events_list, f, ensure_ascii=False, indent=4)
